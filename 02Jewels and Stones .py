42443ae2fbd57702ee85132f8aabc0c9aa82cef1
Sakshi Anand <sakshi.anand@stylumia.com>
2020-05-02 18:52:53 +0530

    Day2

---
+++ 02Jewels and Stones .py
@@ -0,0 +1,11 @@
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        mySet = set(J)
+
+        count = 0
+
+        for c in S:
+            if c in mySet:
+                count += 1
+
+        return count
